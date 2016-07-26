fe2e742a0c9d2b433473316efa8c82b7431d988c
roliveira <roliveira@tcemt-roberto.tcemt.gov.br>
2016-07-26 16:34:07 -0400

    projeto completo

--- projetoTCC/src/util/Configuration.java
+++ projetoTCC/src/util/Configuration.java
@@ -10,7 +10,7 @@ package util;
  * @author roliveira
  */
 public class Configuration { 
-    public static final String SOURCE_DIRECTORY_TO_INDEX = "I:\\backup\\Roberto\\Documentos\\temp";
-    public static final String INDEX_DIRECTORY = "I:\\backup\\Roberto\\Documentos\\temp\\index";
+    public static final String SOURCE_DIRECTORY_TO_INDEX = "I:\\temp";
+    public static final String INDEX_DIRECTORY = "I:\\temp\\index";
     public static final String FIELD_CONTENT = "contents"; // name of the field to index
 }
